d8f28e413c25018cd6b5430b9524894b5246e429
246157 <246157@helium.com>
2025-03-31 11:37:55 +0200

    11. Hermetyzacja nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -10,9 +10,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -27,14 +27,14 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return EMPTY_INDICATOR;
-        return last.value;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
             return EMPTY_INDICATOR;
-        int ret = last.value;
-        last = last.prev;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
 
